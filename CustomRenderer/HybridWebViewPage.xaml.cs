d39e1c877037479e4a71f525c4b2130f792f9c4a
NenadRakas <nenadrakas@outlook.com>
2022-11-06 22:11:24 +0100

    Android switch.

--- CustomRenderer/HybridWebViewPage.xaml.cs
+++ CustomRenderer/HybridWebViewPage.xaml.cs
@@ -1,16 +1,47 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
-using CustomRenderer;
+using System.Threading.Tasks;
 using Xamarin.Forms;
+#if __ANDROID__
+using Android.Graphics;
+#endif
+#if WINDOWS_UWP
+using Windows.Storage.Streams;
+using Windows.Graphics.Imaging;
+using System.Runtime.InteropServices.WindowsRuntime;
+#endif
 namespace CustomRenderer
 {
+    class Threded : ContentPage
+    {
+        private void Thred() 
+        {
+            var hybridWebView = new HybridWebView
+            {
+                Uri = "index.html"
+            };
+            hybridWebView.RegisterAction(data => DisplayAlert("Alert", "Hello " + data, "OK"));
+        } 
+    }
     public partial class HybridWebViewPage : ContentPage
     {
         public HybridWebViewPage()
         {
             InitializeComponent();
-            hybridWebView.RegisterAction(data => DisplayAlert("Alert", "Hello " + data, "OK"));
+            //Task.Delay(TimeSpan.FromSeconds(3.3f));
+            //hybridWebView.RegisterAction(data => DisplayAlert("Alert", "Hello " + data, "OK"));
+            //var hybridWebView = new HybridWebView { Uri = "index.html" };
+            //hybridWebView.RegisterAction(data => DisplayAlert("Alert", "Hello " + data, "OK"));
+            if (Environment.OSVersion.Version.Major >= 10 && Environment.OSVersion.Version.Minor >= 0 && Environment.OSVersion.Version.Build >= 19041)
+            {
+                hybridWebView.RegisterAction(data => DisplayAlert("Alert", "Hello " + data, "OK"));
+            }
+            else
+            {
+                Threded thr = new Threded();
+            }
         }
     }
 }
---
+++ Droid/JSBridge.cs
@@ -0,0 +1,27 @@
+﻿using Android.Webkit;
+using CustomRenderer;
+using Java.Interop;
+using System;
+namespace CustomRenderer.Droid
+{
+    public class JSBridge : Java.Lang.Object
+    {
+        readonly WeakReference<HybridWebViewRenderer> hybridWebViewRenderer;
+
+        public JSBridge(HybridWebViewRenderer hybridRenderer)
+        {
+            hybridWebViewRenderer = new WeakReference<HybridWebViewRenderer>(hybridRenderer);
+        }
+        [JavascriptInterface]
+        [Export("invokeAction")]
+        public void InvokeAction(string data)
+        {
+            HybridWebViewRenderer hybridRenderer;
+
+            if (hybridWebViewRenderer != null && hybridWebViewRenderer.TryGetTarget(out hybridRenderer))
+            {
+                ((HybridWebView)hybridRenderer.Element).InvokeAction(data);
+            }
+        }
+    }
+}
